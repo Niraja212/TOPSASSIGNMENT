885a18a0bf2f50a5aa9919586d2169fab783cb70
Niraja Sheth <npsheth212@gmail.combash>
2025-04-30 10:20:22 +0530

    first commit

---
+++ while_loop_21-4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/* while(condition){
+  //block
+}
+
+Loop has 3 part 1.Intitialization  2. Condition 3. Modification*/
+main(){
+	int i=1;//init
+	while(i<=10){//condition
+		printf("\n i=%d",i);
+		i++;
+	}
+   printf("\n loop over");	
+}
