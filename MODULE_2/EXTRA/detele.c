74ea0dd55c05d443c8082e65f86d8cb148e22a3a
Niraja212 <145531033+Niraja212@users.noreply.github.com>
2025-05-25 17:48:30 +0530

    Update and rename Que5.c to detele.c

--- MODULE_2/EXTRA/detele.c
+++ MODULE_2/EXTRA/detele.c
@@ -1,6 +1,6 @@
 /* Write a C program to check if a number is even or odd using an if-else statement.
 Extend the program using a switch statement to display the month name based on the 
-user’s input (1 for January, 2 for February, etc.). */
+userâ€™s input (1 for January, 2 for February, etc.). */
 
 #include<stdio.h>
 main(){
