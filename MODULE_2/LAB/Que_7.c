6984274d9ceb29c04d15dd6084db804d05cf408e
Niraja212 <145531033+Niraja212@users.noreply.github.com>
2025-05-25 17:31:20 +0530

    Add files via upload

---
+++ MODULE_2/LAB/Que_7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ main(){
+ 	 int i;
+ 	 for(i=1;i<=10;i++){
+ 	 	if(i==5){
+	  	   break;
+	  	   
+		  }
+	    if(i==3){
+	  	   continue;
+	  }
+	  	printf("\n i=%d",i);
+	}
+}
+	  
