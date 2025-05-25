ef983de41938698abfa0e35d817adbe49c039a74
Niraja212 <145531033+Niraja212@users.noreply.github.com>
2025-05-25 17:23:08 +0530

    Add files via upload

---
+++ MODULE_2/LAB/que_3.c
@@ -0,0 +1,31 @@
+//  C program to display variables, constants, comments, and data types.
+//Display their values.
+#include <stdio.h>
+ main() {
+
+    int age;         
+    char grade;     
+    float height;    
+    const int StudentID = 23801234;
+
+    // Input
+    printf("\nEnter the Age: ");
+    scanf("%d", &age);
+
+    getchar();
+
+    printf("\nEnter the Grade: ");
+    scanf("%c", &grade);
+
+    printf("\nEnter the Height (in feet): ");
+    scanf("%0.1f", &height);
+
+    // Output the values
+    printf("\n--- Student Details ---\n");
+    printf("Age: %d\n", age);
+    printf("Grade: %c\n", grade);
+    printf("Height: %.1f feet\n", height);
+    printf("StudentID: %d\n", StudentID);
+
+}
+ 
