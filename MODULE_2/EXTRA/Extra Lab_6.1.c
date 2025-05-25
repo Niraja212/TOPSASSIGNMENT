eac7ef06e6f9835909bf6d3dbf606fb4316ef509
Niraja212 <145531033+Niraja212@users.noreply.github.com>
2025-05-25 18:35:24 +0530

    Add files via upload

---
+++ MODULE_2/EXTRA/Extra Lab_6.1.c
@@ -0,0 +1,28 @@
+/*
+? Write a C program that takes a string as input and reverses it using a function.
+? Challenge: Write the program without using built-in string handling functions.
+*/
+#include <stdio.h>
+
+// Function to reverse a string
+int reverseString(char str[]) {
+    int start = 0, end, temp;
+
+    for (end = 0; str[end] != '\0'; end++);
+    end--;
+
+    while(start < end) {
+        // Swap characters
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
