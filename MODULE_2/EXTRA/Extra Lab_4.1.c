a8990f825b2987496c03039ca308f27279076544
Niraja212 <145531033+Niraja212@users.noreply.github.com>
2025-05-26 09:13:35 +0530

    Update Extra Lab_4.1.c

--- MODULE_2/EXTRA/Extra Lab_4.1.c
+++ MODULE_2/EXTRA/Extra Lab_4.1.c
@@ -4,50 +4,36 @@ the maximum and minimum values in the array.*/
 
 #include<stdio.h>
 main(){
-	int a[10],i;
-	printf("\n enter the array elements");
-	for(i=0;i<10;i++){
-	printf("\n a[%d]=",i);
+	int arr[10],i,j,min,max,temp;
+	printf("\n Enter the 10 integers: ");
+	for(i=0; i<10; i++){
 	scanf("%d",&a[i]);
-    }
-    
-    for(i=0;i<10;i++){
-	scanf("%d",a[i]);
-    }
-    printf("\n ");
-}
-
-/*Extend this to handle a two-dimensional array (3x3 matrix) 
-and calculate the sum of all elements. */
-
-int a[3][3],b[3][3],sum[i][j],i,j;
-
-for(i=0;i<2;i++){
-	 	for(j=0;j<2;j++){
-	 		printf("\n enter a[%d][%d]=",i,j);
-			scanf("%d",&a[i][j]);
-	     	}
-		}
-			printf("\n=========\n");
-			for(i=0;i<2;i++){
-	     	    for(j=0;j<2;j++){
-			printf("\n enter b[%d][%d]=",i,j);
-			scanf("%d",&b[i][j]);
-		    }
-		}
-			printf("\n====addition of array=====\n");
-			for(i=0;i<2;i++){
-	 	    for(j=0;j<2;j++){
-			sum[i][j]= a[i][j] + b[i][j];      		
-			 }
         }
-			
-		for(i=0;i<2;i++){
-	 	for(j=0;j<2;j++){
-	 		printf("\ta[%d][%d]=%d",sum[i][j]);	
-		 }
-		 printf("\n");
-	 }
+	min=a[0];
+	max=a[0];
+	
+	for(i=1; i<10; i++){
+		if(a[i]<min){
+		min=a[i];
+		}
+		if(a[i]>max){
+		max=a[i];
+		}
+	}
+	printf("Minimum element=%d \n",min);
+	printf("Maximum element=%d \n",max);
+	
+	 for(i=0; i<10; i++) {
+         for(j=i+1; j<10; j++) {
+            if(a[i]>a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("Sorted array in ascending order: \n");
+    for(i=0; i<10; i++) {
+        printf("%d ", a[i]);
+    }
 }
-
-
