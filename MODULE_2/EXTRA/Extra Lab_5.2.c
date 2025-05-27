9887678827ad4ea4e2fb2ef079e76ddc26b21d98
Niraja212 <145531033+Niraja212@users.noreply.github.com>
2025-05-27 09:33:45 +0530

    Update Extra Lab_5.2.c

--- MODULE_2/EXTRA/Extra Lab_5.2.c
+++ MODULE_2/EXTRA/Extra Lab_5.2.c
@@ -1,91 +1,51 @@
-/*Write a C program that accepts two 2x2 matrices from the user and adds them. Display the
-resultant matrix.
-Extend the program to work with 3x3 matrices and matrix multiplication.
-*/
+/*Write a C program that calculates the factorial of a given number using a function.
+Challenge: Implement both an iterative and a recursive version of the factorial function 
+and compare their performance for large numbers. */
 
-#include<stdio.h>
-main(){
-	int arr1[2][2],arr2[2][2],c[2][2],i,j,sum[2][2];
-
-    printf("Enter the elements of first 2*2 matrix:\n");
-    for(i=0; i<2; i++) {
-    for(j=0; j<2; j++) {
-        printf("arr1[%d][%d]: ",i,j);
-            scanf("%d",&arr1[i][j]);
-        }
-    }
-
-    printf("Enter elements of second 2*2 matrix:\n");
-    for(i=0; i<2; i++) {
-    for(j=0; j<2; j++) {
-        printf("arr2[%d][%d]: ",i,j);
-            scanf("%d",&arr2[i][j]);
-        }
-    }
-    for(i=0; i<2; i++){
-    for(j=0; j<2; j++){
-    	sum[i][j] = arr1[i][j] + arr2[i][j];
-		}
-	}
-		printf("\nMatrix Addition:\n");
-    for(i=0; i<2; i++) {
-    for(j=0; j<2; j++) {
-        printf("%d\t",sum[i][j]);
-        }
-        printf("\n");
-    }
-
-
-
-//3*3 matrix
-
-#include<stdio.h>
-{
-	int arr1[3][3],arr2[3][3],c[3][3],sum[3][3];
-	int i,j,k;
-
-    printf("Enter elements of first 3*3 matrix:\n");
-    for(i=0; i<3; i++) {
-    for(j=0; j<3; j++) {
-        printf("arr1[%d][%d]: ",i,j);
-            scanf("%d",&arr1[i][j]);
-        }
-    }
-
-    printf("Enter elements of second 3*3 matrix:\n");
-    for(i=0; i<3; i++) {
-    for(j=0; j<3; j++) {
-        printf("arr2[%d][%d]: ",i,j);
-            scanf("%d",&arr2[i][j]);
-        }
-    }
-    for(i=0; i<3; i++){
-    for(j=0; j<3; j++){
-    	sum[i][j]=arr1[i][j]+arr2[i][j];
-		}
-	}
-        for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            c[i][j] = 0;
-            for(k=0; k<3; k++) {
-                c[i][j]+=arr1[i][k]*arr2[k][j];
-            }
-        }
-    }
-    printf("\nMatrix Addition:\n");
-    for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            printf("%d\t",sum[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\nProduct Matrice:\n");
-    for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            printf("%d \t",c[i][j]);
-        }
-        printf("\n");
-        }
-    }
+#include <stdio.h>
+#include <time.h>
+unsigned long long factorialRecursive(int n) {
+    if (n == 0 || n == 1){
+        return 1;}
+    else{
+        return n * factorialRecursive(n - 1);}
 }
+
+unsigned long long factorialIterative(int n) {
+    unsigned long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+    clock_t start, end;
+    double time_recursive, time_iterative;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+        return 1;
+    }
+
+    start = clock();
+    unsigned long long fact_iter = factorialIterative(num);
+    end = clock();
+    time_iterative = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    start = clock();
+    unsigned long long fact_rec = factorialRecursive(num);
+    end = clock();
+    time_recursive = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("\nFactorial of %d (Iterative) = %llu", num, fact_iter);
+    printf("\nTime taken (Iterative): %f seconds", time_iterative);
+    printf("\n\nFactorial of %d (Recursive) = %llu", num, fact_rec);
+    printf("\nTime taken (Recursive): %f seconds\n", time_recursive);
+}
+
 
